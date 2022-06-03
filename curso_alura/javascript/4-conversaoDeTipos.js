e1b7aaa1d288979a5b850bbb358ec1fd379e99f1
delio404 <adelio.a.castro@gmail.com>
2022-06-03 12:59:50 -0300

    comencando curso

---
+++ curso_alura/javascript/4-conversaoDeTipos.js
@@ -0,0 +1,12 @@
+console.log("conversao de tipos")
+
+
+
+
+console.log("ano " + 2022)
+console.log("2" + "2")
+console.log(parseInt("2")+ parseInt("2"))
+console.log("1200"%"2")
+const num =1 + parseInt("32"+"32")
+
+console.log(num)
