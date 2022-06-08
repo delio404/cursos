a6000512c57cd43e1d0cb5236c85a1fa036ce0d5
delio404 <adelio.a.castro@gmail.com>
2022-06-08 14:26:28 -0300

    Repeticao de codigo- classes- atributos-instancia/objeto

---
+++ curso_alura/javascript_orient_obj/index.js
@@ -0,0 +1,31 @@
+
+class Cliente{
+ nome
+ cpf
+ agencia 
+ saldo
+ rg
+
+}
+const cliente1 = new Cliente()
+const cliente2 = new Cliente()
+
+ cliente1.nome =`Goku`
+ cliente1.cpf = 123456129
+ cliente1.agencia = 1001
+ cliente1.saldo= 0
+ cliente1.rg=17171717
+
+ cliente2.nome =`Freddi`
+ cliente2.cpf = 123456129
+ cliente2.agencia = 1001
+ cliente2.saldo= 0
+
+// const cliente2Nome=`Gohan`
+// const cliente2CPF = 123456129
+// const cliente2Agencia = 1001
+// const cliente2Saldo= 0
+
+
+
+console.log("\t ",cliente1,"\n \t",cliente2)
