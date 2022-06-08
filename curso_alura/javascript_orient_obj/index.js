8baadc7cf78db24c8f21b586fe8140f59727e8c9
delio404 <adelio.a.castro@gmail.com>
2022-06-08 11:05:24 -0300

    criando o projeto javascript orientacao a objetos

---
+++ curso_alura/javascript_orient_obj/index.js
@@ -0,0 +1,9 @@
+const cliente1Nome=`Goku`
+const cliente1CPF = 123456129
+const cliente1Agencia = 1001
+const cliente1Saldo= 0;
+
+const cliente2Nome=`Gohan`
+const cliente2CPF = 123456129
+const cliente2Agencia = 1001
+const cliente2Saldo= 0;
